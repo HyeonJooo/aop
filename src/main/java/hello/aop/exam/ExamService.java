190193bf17e29664af84a960b4826ce5ed67d323
park_hyeonju <parkhj2416@gmail.com>
2025-06-21 14:44:26 +0900

    [섹션13] 예제 만들기

---
+++ src/main/java/hello/aop/exam/ExamService.java
@@ -0,0 +1,17 @@
+package hello.aop.exam;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+	private final ExamRepository examRepository;
+
+	public String request(String itemId) {
+		return examRepository.save(itemId);
+	}
+
+}
